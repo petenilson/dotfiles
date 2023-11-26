0d2aba66f144ebc994340524995eaf545eb5f857
Peter Nilson <peter.nilson@byteorbit.com>
2023-11-26 18:05:51 +0200

    reorg

---
+++ .config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,11 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+}
